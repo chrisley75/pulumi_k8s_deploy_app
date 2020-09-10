270fe701a6351c077ed75f382a00ab53f27a32d0
chrisley75 <christopher.ley@gmail.com>
2020-09-10 09:56:48 +0200

    Edit: comment

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 """A Kubernetes Python Pulumi program"""
+#https://www.pulumi.com/docs/get-started/kubernetes/modify-program/
 
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment
 from pulumi_kubernetes.core.v1 import Service
+from pulumi_kubernetes.helm.v3 import Chart, ChartOpts, FetchOpts
 
 app_name = "hello-k8s"
 app_labels = { "app" : "hello-k8s" }
@@ -14,7 +16,7 @@ deployment = Deployment(
         "replicas": 1,
         "template": {
             "metadata": { "labels": app_labels },
-            "spec": { "containers": [{ "name": "hello-k8s", "image": "chrisley75/hello-kubernetes:v1", "env": [{ "name": "MESSAGE", "value": "Super ta presentation Régis !"}] }] }
+            "spec": { "containers": [{ "name": "hello-k8s", "image": "chrisley75/hello-kubernetes:v1", "env": [{ "name": "MESSAGE", "value": "Application deployee et geree avec Pulumi"}] }] }
         }
     })
 
@@ -29,23 +31,5 @@ frontend = Service(
         "ports": [{ "port": 80, "target_port": 8080, "protocol": "TCP" }],
         "selector": app_labels,
     })
-
-
-#https://www.pulumi.com/docs/get-started/kubernetes/modify-program/
-
-
-
-#apiVersion: v1
-#kind: Service
-#metadata:
-#  name: hello-kubernetes-custom
-#spec:
-#  type: LoadBalancer
-#  ports:
-#  - port: 80
-#    targetPort: 8080
-#  selector:
-#    app: hello-kubernetes-custom
-#---
 
 pulumi.export("name", deployment.metadata["name"])
